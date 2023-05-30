ba230f5ae30d22d8e8c7eb86183176b32b1568ba
hamitmizrak <hamitmizrak@gmail.com>
2023-05-30 23:25:12 +0300

    frontend revize

--- template.js
+++ template.js
@@ -5,3 +5,43 @@ Project Name: Blog Page
 -Version: v.1
 -Description: This is project blog Business, my project
 */
+
+// $('body').scrollspy({ target: '#navbar_second' })
+
+// COUNTER
+// $(function () {
+//     $('[data-toggle="counter-up"]').counterUp({
+//         delay: 10,
+//         time: 1000
+//     });
+// });
+
+
+// $(document).ready(function() {
+
+//     var counters = $(".count");
+//     var countersQuantity = counters.length;
+//     var counter = [];
+  
+//     for (i = 0; i < countersQuantity; i++) {
+//       counter[i] = parseInt(counters[i].innerHTML);
+//     }
+  
+//     var count = function(start, value, id) {
+//       var localStart = start;
+//       setInterval(function() {
+//         if (localStart < value) {
+//           localStart++;
+//           counters[id].innerHTML = localStart;
+//         }
+//       }, 40);
+//     }
+  
+//     for (j = 0; j < countersQuantity; j++) {
+//       count(0, counter[j], j);
+//     }
+//   });
+
+// const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navbar_second'
+//   })
